71237f0fcf6cb261bd7b0cc612e8f06a72d13ba1
dserafim1999 <dserafim1999@gmail.com>
2019-12-05 14:43:32 +0000

    day 2 part 2

--- Day2/part2.py
+++ Day2/part2.py
@@ -1,19 +1,29 @@
-'''with open("input.txt") as file:
+def intcodeProgram(code):
+    i = 0
+    while  not code[i] == 99:
+        if code[i] == 1:
+            code[code[i+3]] = code[code[i+1]] + code[code[i+2]]
+            i += 4   
+        elif code[i] == 2:
+            code[code[i+3]] = code[code[i+1]] * code[code[i+2]]
+            i += 4   
+        elif code[i] == 99:
+            exit()
+        else:
+            print("Something went wrong")
+            exit()
+    return code[0]
+#---------------------------------------------------#
+
+with open("input.txt") as file:
     text = file.read()
 
 code = [int(x) for x in text.split(',')]
-code[1], code[2], i = 12,2,0
 
-while  not code[i] == 99:
-    if code[i] == 1:
-        code[code[i+3]] = code[code[i+1]] + code[code[i+2]]
-        i += 4   
-    elif code[i] == 2:
-        code[code[i+3]] = code[code[i+1]] * code[code[i+2]]
-        i += 4   
-    elif code[i] == 99:
-        exit()
-    else:
-        print("Something went wrong")
-        exit()
-print(code[0])'''
+for noun in range(0,100):
+    for verb in range(0,100):
+        auxCode = code.copy()
+        auxCode[1], auxCode[2] = noun, verb
+        if intcodeProgram(auxCode) == 19690720:
+            print(100*noun+verb)
+            exit()
