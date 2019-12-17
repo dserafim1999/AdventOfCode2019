5797e79e84938f6c0502030a7cff8960016f2be1
dserafim1999 <dserafim1999@gmail.com>
2019-12-17 12:49:01 +0000

    day 9

--- Day9/part1.py
+++ Day9/part1.py
@@ -2,21 +2,24 @@
 def instructions(modes, instruction, i):
     parameters, j = [], 1
     global relative
-    for mode in modes:
-        if mode == 0: parameters.append(code[checkIndex(code, code[i+j])]) #j is the offset in the instruction, meaning for example that i+1 would be the parameter straight after the opcode
-        elif mode == 1: parameters.append(code[checkIndex(code, i+j)])
-        elif mode == 2: parameters.append(code[checkIndex(code, code[i+j]+relative)])
+    for mode in modes: #j is the offset in the instruction, meaning for example that i+1 would be the parameter straight after the opcode
+        if mode == 0: 
+            if j < 3: parameters.append(code[checkIndex(code, code[i+j])])
+            elif j == 3: parameters.append(code[i+j])
+        elif mode == 1: parameters.append(code[i+j])
+        elif mode == 2: 
+            if j < 3: parameters.append(code[checkIndex(code, code[i+j] + relative)])
+            elif j == 3: parameters.append(code[i+j] + relative)
         j += 1
 
     if instruction == 1: 
-        code[checkIndex(code, code[i+3])] = parameters[0] + parameters[1]
+        code[checkIndex(code, parameters[2])] = parameters[0] + parameters[1]
     elif instruction == 2: 
-        code[checkIndex(code, code[i+3])] = parameters[0] * parameters[1]
+        code[checkIndex(code, parameters[2])] = parameters[0] * parameters[1]
     elif instruction == 3:
         value = int(input())
-        if modes[0] == 0: code[checkIndex(code, code[i+1])] = value
-        elif modes[0] == 1: code[checkIndex(code, i+1)] = value
-        elif modes[0] == 2: code[checkIndex(code, code[i+1]+relative)] = value
+        if modes[0] in (0,1): code[checkIndex(code, code[i+1])] = value
+        elif modes[0] == 2: code[checkIndex(code,code[i+1]+relative)] = value
     elif instruction == 4:
         print(parameters[0])
     elif instruction == 5:
@@ -26,11 +29,11 @@ def instructions(modes, instruction, i):
         if parameters[0] == 0: return parameters[1]
         else: return i+3
     elif instruction == 7:
-        if parameters[0] < parameters[1]: code[checkIndex(code, code[i+3])] = 1
-        else: code[checkIndex(code, code[i+3])] = 0
+        if parameters[0] < parameters[1]: code[checkIndex(code, parameters[2])] = 1
+        else: code[checkIndex(code, parameters[2])] = 0
     elif instruction == 8: 
-        if parameters[0] == parameters[1]: code[checkIndex(code, code[i+3])] = 1
-        else: code[checkIndex(code, code[i+3])] = 0
+        if parameters[0] == parameters[1]: code[checkIndex(code, parameters[2])] = 1
+        else: code[checkIndex(code, parameters[2])] = 0
     elif instruction == 9:
         relative += parameters[0]
 
@@ -53,10 +56,9 @@ i, relative = 0,0
 while not code[i] == 99:
     modes = [int(i) for i in str(code[i]//100)]
     instruction =  code[i]%100
-    while len(modes) != 2 and not instruction in (3,4,9):
+    while len(modes) < 3 and not instruction in (3,4):
         modes.insert(0,0)    
     modes.reverse()
-
     if instruction in (1,2,7,8):
         instructions(modes, instruction, i)
         i += 4   
